3f18392c3fd431f120a8f2c456551a8a0ccf3559
Marina <venskaanika@gmail.com>
2024-10-28 16:32:44 +0200

    homework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿//1 zadanie
+using System;
+
 int n = 0;
 do
 {
@@ -8,12 +10,12 @@ do
 
 
 //2 zadanie
-string password = "";
-do
-{
-    Console.WriteLine("podaj haslo :) ");
-    password = Console.ReadLine();
-} while (password != "a");
+//string password = "";
+//do
+//{
+//    Console.WriteLine("podaj haslo :) ");
+//    password = Console.ReadLine();
+//} while (password != "a");
 
 //3 zadanie
 int a = 1;
@@ -72,23 +74,149 @@ for (int i = 0; i < 20; i++)
 //    }
 //}
 
-int[,] m = new int[3,4];
+//int[,] m = new int[3,4];
+//int value = 1;
+//for (int i = 0; i < 3; i++)
+//{
+//    for (int j = 0; j < 4; j++)
+//    {
+//        m[i,j] = value;
+//        value++;
+//    }
+//}
+//Console.WriteLine("Macierz po transpozycji:");
+//for (int i = 0;i < 4; i++)
+//{
+//    for (int j = 0; j < 3; j++)
+//    {
+//        Console.WriteLine(m[i,j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+int[,] matrix = new int[3, 4];
+
 int value = 1;
 for (int i = 0; i < 3; i++)
 {
     for (int j = 0; j < 4; j++)
     {
-        m[i,j] = value;
+        matrix[i, j] = value;
         value++;
     }
 }
+
+// Wyświetlenie macierzy po transpozycji
 Console.WriteLine("Macierz po transpozycji:");
-for (int i = 0;i < 4; i++)
+for (int i = 0; i < 4; i++)
 {
     for (int j = 0; j < 3; j++)
     {
-        Console.WriteLine(m[i,j] + " ");
+        Console.Write(matrix[j, i] + " ");
     }
     Console.WriteLine();
 }
 
+//Koniec Zadania 8/9
+
+//Zadanie 10 
+//Zdefiniuj procedurę , która przyjmie jako paramter ciąg znaków i wypisze go na ekran, ale WIELKIMI LITERAMI. 
+PrintUpperCase("Yaaay! Hello world");
+static void PrintUpperCase(string input)
+{
+    Console.WriteLine(input.ToUpper());
+}
+
+//Zadanie 11
+void PrintConsoleSquare(int x, int y)
+{
+    //rysujemy pierwszy wiersz
+    for(int i = 0; i < x; i++)
+        Console.Write("$");
+    //schodzimy do nowej linii
+    Console.WriteLine();
+    //rysujemy srodek (boki)
+    for(int i = 0; i < y; i++)
+    {
+        //lewy bok rysujemy
+        Console.Write("$");
+
+        //puste miejsce
+        for(int j = 0;j < x - 2; j++)
+        {
+            Console.Write(" ");
+        }
+
+        //prawy bok
+        Console.Write("$");
+        //zjazd do nowej linii
+        Console.WriteLine(" ");
+    }
+    //dolny wiersz
+    for(int i = 0;i < x; i++)
+        Console.Write("$");
+}
+PrintConsoleSquare(5, 5);
+
+//zadanie 12
+static string EvenOrOdd1(int number)
+{
+  string retStr = " ";
+    if (number % 2 == 0)
+    {
+        retStr = "Even";
+    }
+    else
+    {
+        retStr = "Odd";
+    }
+    return retStr;
+    
+}
+
+//13 zadanie
+Console.WriteLine(" ");
+
+int b = 5;
+int m = 4;
+
+char[,] tablica = WypełnijTablice(b, m);
+WyswietlTabliceNaEkran(tablica);
+
+static char[,] WypełnijTablice(int b, int m)
+{
+    char[,] tablica = new char[b, m];
+
+    for (int i = 0; i < b; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i == 0 || i == b - 1 || j == 0 || j == m - 1)
+            {
+                tablica[i, j] = '#';
+            }
+            else
+            {
+                tablica[i, j] = ' ';
+            }
+        }
+    }
+    return tablica;
+}
+
+// Функция для вывода таблицы на экран
+static void WyswietlTabliceNaEkran(char[,] tablica)
+{
+    int b = tablica.GetLength(0);
+    int m = tablica.GetLength(1);
+
+    for (int i = 0; i < b; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write(tablica[i, j]);
+        }
+        Console.WriteLine(); // Переход на новую строку после каждого ряда
+    }
+}
+// Koniec Zadanie 13
